f44d3db4903e18373e7f9ba39eebd0a5fbe9dd8f
Simon <45477631+nomisc1@users.noreply.github.com>
2019-01-28 14:41:26 +0700

    Final update (working form)

--- Contact.js
+++ Contact.js
@@ -21,22 +21,41 @@ class ContactDetails extends React.Component {
   }
 
   render() {
-    return (
-      <div id="authorization">
-        <h1>Contact</h1>
-        <ul>
+    // display login form when auth=false  
+    const login = (
+    	<form action="#" 
+      onSubmit="{this.authorize}">
+      <input
+      type="password"
+      placeholder="password"/>
+      <input
+      type="submit"/>
+      </form>
+    );
+    
+    // display contact information when auth=true
+    const contactInfo = (
+          <ul>
           <li>
             josh@apple.com
           </li>
           <li>
             036587282
           </li>
-        </ul>
+          </ul>
+    );
+    
+    return (
+      <div id="authorization">
+      // the operation displays Login for default state (false)
+        <h1>{this.state.authorized ? 'Login' : 'Enter the Password'</h1>
+      {this.state.authorized ? login : contactInfo}
       </div>
     );
   }
 }
 
+// render ContactDetails instance
 ReactDOM.render(
   <ContactDetails />, 
   document.getElementById('app')
